7f3fe8e5ae08c07320d4e682a344a64c942b75e3
Makhmud Mustafabek Galy <makhmudgaly2@gmail.com>
2023-04-24 07:22:44 +0700

    Time: 66 ms (8.55%), Space: 42.4 MB (29.06%) - LeetHub

---
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -0,0 +1,17 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let a = 0;
+    let b = 1;
+    while(true) {
+        yield a;
+        [a,b] = [b, b+a];
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
