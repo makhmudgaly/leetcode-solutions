a8c209958767e0b6136f0b4551b4029bff3933e4
Makhmud Mustafabek Galy <makhmudgaly2@gmail.com>
2024-08-08 01:31:54 +0700

    Time: 26 ms (95.49%), Space: 16.5 MB (14.10%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo ={}
+        memo[1] = 1
+        memo[2] = 2
+        
+        def climb(n):
+            if n in memo: # if the recurssion already done before first take a look-up in the look-up table
+                return memo[n]
+            else:   # Store the recurssion function in the look-up table and reuturn the stored look-up table function
+                memo[n] =  climb(n-1) + climb(n-2)
+                return memo[n]
+        
+        return climb(n)
