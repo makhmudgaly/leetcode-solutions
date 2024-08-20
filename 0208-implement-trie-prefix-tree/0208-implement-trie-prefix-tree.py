f9794e1b37b0c94ad03c9036fc253a2e1a3e3d44
Makhmud Mustafabek Galy <makhmudgaly2@gmail.com>
2024-08-20 08:16:50 +0700

    Time: 98 ms (97.34%), Space: 28.8 MB (90.45%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -0,0 +1,27 @@
+class Trie(object):
+
+    def __init__(self):
+        self.trie = {}
+
+
+    def insert(self, word):
+        t = self.trie
+        for c in word:
+            if c not in t: t[c] = {}
+            t = t[c]
+        t["-"] = True
+
+
+    def search(self, word):
+        t = self.trie
+        for c in word:
+            if c not in t: return False
+            t = t[c]
+        return "-" in t
+
+    def startsWith(self, prefix):
+        t = self.trie
+        for c in prefix:
+            if c not in t: return False
+            t = t[c]
+        return True
