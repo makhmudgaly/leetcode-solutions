1c6c506e3555637169f36e08c2992dd14f42cc51
Makhmud Mustafabek Galy <makhmudgaly2@gmail.com>
2025-05-18 20:49:00 +0700

    Time: 2793 ms (14.56%), Space: 18 MB (63.47%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -0,0 +1,59 @@
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        n = len(board)
+
+        rows, cols, boxes = collections.defaultdict(set), collections.defaultdict(set), collections.defaultdict(set)
+
+        for r in range(n):
+            for c in range(n):
+                if board[r][c] == '.':
+                    continue
+
+                v = int(board[r][c])
+                rows[r].add(v)
+                cols[c].add(v)
+                boxes[(r // 3) * 3 + c // 3].add(v)
+
+
+        def is_valid(r, c, v):
+            box_id = (r // 3) * 3 + c // 3
+            return v not in rows[r] and v not in cols[c] and v not in boxes[box_id]
+
+
+        def backtrack(r, c):
+            if r == n - 1 and c == n:
+                return True
+            elif c == n:
+                c = 0
+                r += 1
+
+            # current grid has been filled
+            if board[r][c] != '.':
+                return backtrack(r, c + 1)
+
+            box_id = (r // 3) * 3 + c // 3
+            for v in range(1, n + 1):
+                if not is_valid(r, c, v):
+                    continue
+
+                board[r][c] = str(v)
+                rows[r].add(v)
+                cols[c].add(v)
+                boxes[box_id].add(v)
+
+                if backtrack(r, c + 1):
+                    return True
+
+                # backtrack
+                board[r][c] = '.'
+                rows[r].remove(v)
+                cols[c].remove(v)
+                boxes[box_id].remove(v)
+
+            return False
+
+
+        backtrack(0, 0)
